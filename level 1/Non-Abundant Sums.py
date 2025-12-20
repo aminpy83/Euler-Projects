516ccbe744eccc62f94b71d021b4c8e5451589a2
amin <aminpyss@gmail.com>
2025-12-20 22:25:45 +0330

    done but too slow ( O(n^3) )

---
+++ level 1/Non-Abundant Sums.py
@@ -0,0 +1,27 @@
+# a + b = c
+# a , b are abundant
+#smallestabundant is 12
+
+def is_abundant(n):
+    divs_sum = 1
+    for i in range(2, n // 2 + 1):
+        if n % i == 0:
+            divs_sum += i
+    if n <= divs_sum:
+        return True
+    return False
+
+def c_finder(num):      # return c if it couldn't find the two abundant
+    for i in range(12, num // 2 + 1):
+        if is_abundant(i) and  is_abundant(num - i):
+            return False
+    return num
+
+
+last = 28123
+total_sum = sum(range(13))
+for x in range(13, last+1):
+    if c_finder(x):
+        total_sum += x
+        print(x)
+print(total_sum)
