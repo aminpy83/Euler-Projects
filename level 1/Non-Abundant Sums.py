331b17dca4b6de2b02c90bf0ee35093b4cdc2864
amin <aminpyss@gmail.com>
2025-12-20 23:18:00 +0330

    caching added

--- level 1/Non-Abundant Sums.py
+++ level 1/Non-Abundant Sums.py
@@ -2,18 +2,24 @@
 # a , b are abundant
 #smallestabundant is 12
 
+abundant_cache = set()
 def is_abundant(n):
+    if n in abundant_cache:
+        return True
     divs_sum = 1
     for i in range(2, n // 2 + 1):
         if n % i == 0:
             divs_sum += i
     if n <= divs_sum:
+        abundant_cache.add(n)
         return True
     return False
 
 def c_finder(num):      # return c if it couldn't find the two abundant
     for i in range(12, num // 2 + 1):
-        if is_abundant(i) and  is_abundant(num - i):
+        if i in abundant_cache and  (num - i) in abundant_cache:
+            return False
+        elif is_abundant(i) and  is_abundant(num - i):
             return False
     return num
 
